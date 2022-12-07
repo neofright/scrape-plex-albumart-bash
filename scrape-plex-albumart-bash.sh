9caa71de6f009ea44ee3013698512527e4d7c0ba
neofright <68615872+neofright@users.noreply.github.com>
2022-12-07 14:42:37 +0000

    Use faster SQL instead of subprocesses
    - Single SQL query is presumed to be faster than 3 subprocesses
    - Tidy up comments

--- scrape-plex-albumart-bash.sh
+++ scrape-plex-albumart-bash.sh
@@ -15,31 +15,20 @@ do
         album_art_base_dir="${plex_dir}/Metadata/Albums/${metadata_items_album_hash:0:1}/${metadata_items_album_hash:1}.bundle/Contents/_combined"
         local_album_art="${album_art_base_dir}/${metadata_items_album_thumb_url#*/}"
 
-        ## Get the first track id of this album
-        ## Lookup the track's metadata_item_id and then retrieve the location of the track on disk
-        child_id="$(sqlite3 "$plex_db" "SELECT id FROM metadata_items WHERE parent_id = '$metadata_items_album_id' LIMIT 1")"
-        track_media_id="$(sqlite3 "$plex_db" "SELECT id FROM media_items WHERE metadata_item_id = '$child_id'")"
-        track_file="$(sqlite3 "$plex_db" "SELECT file FROM media_parts WHERE media_item_id = '$track_media_id'")"
-
-        ## The album directory is where the track file is stored
-        album_dir="$(dirname "$track_file")"
+        ## Get the file location of this album's first track
+        track_file="$(sqlite3 "$plex_db" "SELECT media_parts.file FROM metadata_items JOIN media_items ON media_items.metadata_item_id = metadata_items.id JOIN media_parts ON media_parts.media_item_id = media_items.id WHERE metadata_items.parent_id = '$metadata_items_album_id' LIMIT 1;")"
+        album_dir="$(dirname "$track_file")" ## The album directory is where the track file is stored
 
         ## I don't want to store the album art in the original directory tree, so I do path substitution here
         album_dir_replaced="${album_dir//\/mnt\/Music\//\/mnt\/tank\/data\/Music\/Album Art\/}"
         mkdir -p "${album_dir_replaced}"
         
         destination_file="${album_dir_replaced}/folder.jpg"
-        ## If we haven't already scraped this album's art...
-        if [[ ! -f "$destination_file" ]]
-        then
-            ## - Convert the file format to jpeg if it is in another format.
-            ## - Resize the album art. This is being used on an iPod (the display is 320x320).
-            ## - Rockbox doesn't support progressive scan jpegs, so we must convert the image.
-            ## https://stackoverflow.com/questions/14556984/imagemagick-creating-multiple-files
-            printf "Converting and resizing %s..." "$destination_file"
-            convert "$local_album_art"[0] -resize 320x320 -interlace none "$destination_file" 2>/dev/null && \
-            printf "OK\n" || \
-            printf "ERR\n"
-        fi
+        ## If we haven't already scraped this album's artwork:
+        ##      - Convert the file format to jpeg if it is in another format.
+        ##      - Resize the album art as this is being used on an iPod where the display is only 320x320.
+        ##      - Rockbox doesn't support progressive scan jpegs, so we must convert the image.
+        ## https://stackoverflow.com/questions/14556984/imagemagick-creating-multiple-files
+        if [[ ! -f "$destination_file" ]]; then printf "Converting and resizing %s..." "$destination_file"; convert "$local_album_art"[0] -resize 320x320 -interlace none "$destination_file" 2>/dev/null && printf "OK\n" || printf "ERR\n"; fi
     fi
 done < <(sqlite3 "$plex_db" "SELECT id,hash,user_thumb_url FROM metadata_items WHERE metadata_type = '9' AND library_section_id != '10'")
