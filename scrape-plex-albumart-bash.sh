a652b913780eaf151d9969b3056ff07035ad8326
neofright <68615872+neofright@users.noreply.github.com>
2022-12-07 15:08:21 +0000

    Switch to a function and generic library names
    - More dynamically scrape music libraries containing the string 'Music'

--- scrape-plex-albumart-bash.sh
+++ scrape-plex-albumart-bash.sh
@@ -6,24 +6,36 @@ if [[ "$deps" -ne 0 ]]; then echo "ERROR: Install the $deps missing dependencies
 plex_dir="/mnt/scratch/plex/Library/Application Support/Plex Media Server"
 plex_db="${plex_dir}/Plug-in Support/Databases/com.plexapp.plugins.library.db"
 
-while IFS="|" read -r metadata_items_album_id metadata_items_album_hash metadata_items_album_thumb_url
+function scrape_plex_album_art()
+{
+    library_section_id="$1"
+    while IFS="|" read -r metadata_items_album_id metadata_items_album_hash metadata_items_album_thumb_url
+    do
+        ## Some albums simply don't have artwork available, so we should skip them!
+        if [[ -n "$metadata_items_album_thumb_url" ]]
+        then
+            ## Build the full path to the album art on disk...
+            album_art_base_dir="${plex_dir}/Metadata/Albums/${metadata_items_album_hash:0:1}/${metadata_items_album_hash:1}.bundle/Contents/_combined"
+            local_album_art="${album_art_base_dir}/${metadata_items_album_thumb_url#*/}"
+
+            ## Get the file location of this album's first track
+            track_file="$(sqlite3 "$plex_db" "SELECT media_parts.file FROM metadata_items JOIN media_items ON media_items.metadata_item_id = metadata_items.id JOIN media_parts ON media_parts.media_item_id = media_items.id WHERE metadata_items.parent_id = '$metadata_items_album_id' LIMIT 1;")"
+            album_dir="$(dirname "$track_file")" ## The album directory is where the track file is stored
+
+            ## I don't want to store the album art in the original directory tree, so I do path substitution here
+            album_dir_replaced="${album_dir//\/mnt\/Music\//\/mnt\/tank\/data\/Music\/Album Art\/}"
+            mkdir -p "${album_dir_replaced}"
+            
+            destination_file="${album_dir_replaced}/folder.jpg"
+            if [[ ! -f "$destination_file" ]];then printf "Converting and resizing %s..." "$destination_file";convert "$local_album_art"[0] -resize 320x320 -interlace none "$destination_file" 2>/dev/null && printf "OK\n" || printf "ERR\n";fi
+        fi
+    done < <(sqlite3 "$plex_db" "SELECT id,hash,user_thumb_url FROM metadata_items WHERE metadata_type = '9' AND library_section_id = '$library_section_id'")
+}
+
+## Your Plex library name must contain the string "Music".
+## There is enough example code here to easily change this if your libraries are named differently.
+while IFS="|" read -r library_id library_name
 do
-    ## Some albums simply don't have artwork available, so we should skip them!
-    if [[ -n "$metadata_items_album_thumb_url" ]]
-    then
-        ## Build the full path to the album art on disk...
-        album_art_base_dir="${plex_dir}/Metadata/Albums/${metadata_items_album_hash:0:1}/${metadata_items_album_hash:1}.bundle/Contents/_combined"
-        local_album_art="${album_art_base_dir}/${metadata_items_album_thumb_url#*/}"
-
-        ## Get the file location of this album's first track
-        track_file="$(sqlite3 "$plex_db" "SELECT media_parts.file FROM metadata_items JOIN media_items ON media_items.metadata_item_id = metadata_items.id JOIN media_parts ON media_parts.media_item_id = media_items.id WHERE metadata_items.parent_id = '$metadata_items_album_id' LIMIT 1;")"
-        album_dir="$(dirname "$track_file")" ## The album directory is where the track file is stored
-
-        ## I don't want to store the album art in the original directory tree, so I do path substitution here
-        album_dir_replaced="${album_dir//\/mnt\/Music\//\/mnt\/tank\/data\/Music\/Album Art\/}"
-        mkdir -p "${album_dir_replaced}"
-        
-        destination_file="${album_dir_replaced}/folder.jpg"
-        if [[ ! -f "$destination_file" ]];then printf "Converting and resizing %s..." "$destination_file";convert "$local_album_art"[0] -resize 320x320 -interlace none "$destination_file" 2>/dev/null && printf "OK\n" || printf "ERR\n";fi
-    fi
-done < <(sqlite3 "$plex_db" "SELECT id,hash,user_thumb_url FROM metadata_items WHERE metadata_type = '9' AND library_section_id != '10'")
+    printf "Scraping artwork from Plex library '%s'...\n" "$library_name"
+    scrape_plex_album_art "$library_id"
+done < <(sqlite3 "$plex_db" "SELECT id,name FROM library_sections WHERE name LIKE '%Music%' AND section_type = '8'")
